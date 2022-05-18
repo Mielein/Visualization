f6dffca2915424705085b76d15de37e9ad223789
Mielein <marie.trojan@uni-weimar.de>
2022-05-19 00:49:18 +0200

    kein bock mehr

--- Visualisierung/A4/index.js
+++ Visualisierung/A4/index.js
@@ -1,7 +1,8 @@
 import * as d3 from "d3"
+import { selection } from "d3";
 
 const data = await d3.csv("data/iris.csv", d3.autoType);
-console.log(data);
+data.forEach((d, i) => d.index = i);
 
 const width = 800;
 const height = width;
@@ -10,87 +11,118 @@ const padding = 20;
 
 const parent = d3.select("#visualization");
 const svg = parent.append("svg")
-.attr("viewBox", [0,0,width,height]);
+.attr("viewBox", [-30,0,width,height]);
 
-const crossProduct = d3.cross(data.columns, data.columns).filter(x => !x.includes("species"));
-
-for(let i = 0; i < crossProduct.length; i++){
-    scatterPlot(svg, data, crossProduct[i][0], crossProduct[i][1], "species", width, height, margin, i);
-
-}
+const x = data.columns.filter(x => !x.includes("species"));;
+const y = data.columns.filter(x => !x.includes("species"));;
 
 
-
-function scatterPlot(parent, data, x, y, colour, width, height, margin, i){
-
-    const matrixWidth = (width - margin.left - margin.right - 3 * padding) / 4;
-    const matrixHeight = (height - margin.top - margin.bottom - 3 * padding) / 4;
+scatterPlot(svg, data, x, y, {species: d => d.species}, width, height, margin);
 
 
-    const scaleX = d3.scaleLinear()
-    .domain(d3.extent(data, d => d[x])) //extend() finds out highest or lowest value of array 
-    .range([0, matrixWidth])
-    .nice();
+function scatterPlot(parent, data, x, y, {species = () => 1}, width, height, margin){
+    const X = d3.map(x, x => d3.map(data, typeof x === "function" ? x : d => d[x]));
+    const Y = d3.map(y, y => d3.map(data, typeof y === "function" ? y : d => d[y]));
+    const S = d3.map(data, species);
 
-    const scaleY = d3.scaleLinear()
-    .domain(d3.extent(data, d => d[y])) 
-    .range([matrixHeight, 0])
-    .nice();
-    const scaleColour = d3.scaleOrdinal(d3.schemePaired); //do not have to define input domain
+    const matrixWidth = (width - margin.left - margin.right - (X.length - 1) * padding) / X.length;
+    const matrixHeight = (height - margin.top - margin.bottom - (Y.length - 1) * padding) / Y.length;
 
-    if(i % 4 == 0){
-        if(i == 0){
-            parent.append("g") //groupelement
-            .attr("transform", `translate(0 , ${(matrixHeight + padding)})`)
-            .call(d3.axisBottom(scaleX));
-            
-        }
+    const scaleColour = d3.schemePaired;
 
-        parent.append("g") //groupelement
-        .attr("transform", `translate(0 , ${i * (matrixHeight + padding)})`)
-        .call(d3.axisBottom(scaleX));
+    const scaleX = X.map(X => d3.scaleLinear(d3.extent(X), [0, matrixWidth]));
+    const scaleY = Y.map(Y => d3.scaleLinear(d3.extent(Y), [matrixHeight, 0]));
+    const scaleS = d3.scaleOrdinal(S, scaleColour);
 
-        parent.append("g") 
-        .attr("transform", `translate(${( padding)}, ${matrixHeight + padding})`)
-        .call(d3.axisLeft(scaleY));
+    const axisX = d3.axisBottom().ticks(6);
+    const axisY = d3.axisLeft().ticks(6);
+    
+    parent.append("g") //groupelement
+    .selectAll("g")
+    .data(scaleX)
+    .join("g")
+    .attr("transform", (d, i) => `translate(${i * (matrixWidth + padding)},${height - margin.bottom - margin.top})`)
+    .each(function(scaleX) { return d3.select(this).call(axisX.scale(scaleX)); });
 
-/*         parent.append("g") //groupelement
-        .attr("transform", `translate(${(i/4) * (matrixWidth + padding)} , ${i * matrixHeight + padding})`)
-        .call(d3.axisBottom(scaleX));
+    parent.append("g") 
+    .selectAll("g")
+    .data(scaleY)
+    .join("g")
+    .attr("transform", (d, i) => `translate(0,${i * (matrixHeight + padding)})`)
+    .each(function(scaleY) { return d3.select(this).call(axisY.scale(scaleY)); });
 
-        parent.append("g") 
-        .attr("transform", `translate(${i/4 * (matrixHeight + padding)}, ${i * matrixHeight + padding})`)
-        .call(d3.axisLeft(scaleY)); */
-    }
-    else{
-        parent.append("g") //groupelement
-        .attr("transform", `translate(${i * (matrixWidth + padding)} , ${matrixHeight + padding})`)
-        .call(d3.axisBottom(scaleX));
+    const matrix = parent.append("g")
+    .selectAll("g")
+    .data(d3.cross(d3.range(X.length), d3.range(Y.length)))
+    .join("g")
+    .attr("transform", ([i, j]) => `translate(${i * (matrixWidth + padding)},${j * (matrixHeight + padding)})`);
 
-        parent.append("g") 
-        .attr("transform", `translate(${i * (matrixWidth + padding)}, ${matrixHeight + 15})`)
-        .call(d3.axisLeft(scaleY));
-    }
+    matrix.append("rect")
+    .attr("fill", "none")
+    .attr("stroke", "currentColor")
+    .attr("width", matrixWidth)
+    .attr("height", matrixHeight)
 
-    parent.selectAll("circle")
-    .data(data)
-    .join("circle")
-    .attr("cx", d => scaleX(d[x])) //central x
-    .attr("cy", d => scaleY(d[y])) //central y
-    .attr("r", 5) //radius
-    .attr("fill", d => scaleColour(d[colour]));
+    console.log(d => scaleColour(d.species));
+    matrix.each(function([x,y]) {
+        d3.select(this).selectAll("circle")
+        .data(d3.range(S.length))
+        .join("circle")
+        .attr("cx", i => scaleX[x](X[x][i])) //central x
+        .attr("cy", i => scaleX[y](Y[y][i])) //central y
+
+    });
+
+    const circle = matrix.selectAll("circle")
+    .attr("r", 2) //radius
+    .attr("fill", i => scaleS(S[i]));
+
+    if (x === y) svg.append("g")
+    .attr("font-size", 10)
+    .selectAll("text")
+    .data(x)
+    .join("text")
+    .attr("transform", (d, i) => `translate(${i * (matrixWidth + padding)},${i * (matrixHeight + padding)})`)
+    .attr("x", padding / 2)
+    .attr("y", padding / 2)
+    .attr("dy", ".3em") 
+    .text(d => d);
 
     const brush = d3.brush()
-    .extent([[margin.left, margin.top], [width - margin.right, height - margin.bottom]]) //upper left corner and bottom right corner
-    .on("start brush end", brushed);
+    .extent([[ 0, 0], [matrixWidth, matrixHeight]]) //upper left corner and bottom right corner
+    .on("start", brushBeginn)
+    .on("brush", brushed)
+    .on("end", brushEnded );
 
-    parent.call(brush);
+    matrix.call(brush);
 
-    function brushed({selection}, data){
-        console.log(selection, data);
-        if(selection && selection[0] && selection[1]){
-            console.log("x", scaleX.invert(selection[0][0]),scaleX.invert(selection[1][0]))
-            console.log("y", scaleY.invert(selection[0][1]),scaleY.invert(selection[1][1]))
+    let brushMatrix;
+
+    function brushBeginn() {
+        if(brushMatrix !== this) {
+            d3.select(brushMatrix).call(brush.move, null);
+            brushMatrix = this;
         }
     }
+        
+    function brushed({selection}, [i,j]){
+        let selected = []
+        /* if(selection && selection[0] && selection[1]){
+            console.log("x", scaleX.invert(selection[0][0]),scaleX.invert(selection[1][0]))
+            console.log("y", scaleY.invert(selection[0][1]),scaleY.invert(selection[1][1]))
+        } */
+        parent.property("value", selected).dispatch("input");
+
+    }    
+    function brushEnded({selection}) {
+        if(selection) return;
+        parent.property("value", []).dispatch("input");
+        circle.classed("hidden", false);
+    }   
 }
+
+//used 
+//https://observablehq.com/@d3/brushable-scatterplot-matrix
+//https://observablehq.com/@d3/splom
+//also got help from Lisa Piekarski
+
--- Visualisierung/A4/npmstuff.sh
+++
@@ -1,3 +0,0 @@
-npm install --save-dev snowpack &&
-npm run start &&
-sudo apt install vega vega-lite vega-lite-api
