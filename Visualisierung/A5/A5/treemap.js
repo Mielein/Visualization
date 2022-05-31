7c32aa59036c6f2325939cf5428e99f4f3b8d0a4
Mielein <marie.trojan@uni-weimar.de>
2022-05-31 22:25:49 +0200

    started on treemaps

--- Visualisierung/A5/A5/treemap.js
+++ Visualisierung/A5/A5/treemap.js
@@ -40,21 +40,58 @@ export function treemap({
 
     // TODO: prepare the treemap using d3.treemap and d3.hierarchy with the
     // selected tiling algorithm. 
-    
+    const hierarchy = d3
+    .hierarchy(data)
+    .sum((d) => d.revenue)
+    .sort((a, b) => b.height - a.height || b.value - a.value);
+    const root = d3.partition().size([height, width]).padding(1)(hierarchy);
+    const maxDepth = d3.max(root.descendants(), (d) => d.depth);
+    const x = d3
+      .scaleBand()
+      .domain(d3.range(1, maxDepth + 1))
+      .range([0, width]);
 
     d3.treemap() 
       .size([width, height])
+      .padding(1)
+      (root)
 
-    
+    console.log(root.leaves())
     draw();
 
     // TODO: finish the draw function. You can take inspiration (and code) from 
     // the given icicles implementation for both the rectangles and the labels.
     // Please, do not forget the tooltips. 
     function draw() {
-      // TODO: create a group for each leaf node
+      // TODO: create a group for each leaf node#
+      
       // TODO: draw a rectangle
+      svg
+        .selectAll("rect")
+        .data(root.leaves())
+        .enter()
+        .append("rect")
+          .attr('x', (d) => d.x0)
+          .attr('y', (d) => d.y0)
+          .attr('width', (d) => d.x1 - d.x0)
+          .attr('height', (d) => d.y1 - d.y0)
+          .style("stroke", "black")
+          .attr("fill", (d) => {
+            if (!d.depth) return "#ccc";
+            while (d.depth > 1) d = d.parent;
+            return color(d.data.name);
+          });
       // TODO: draw the label
+      svg
+        .selectAll("text")
+        .data(root.leaves())
+        .enter()
+        .append("text")
+          .attr("x", (d) => d.x0)
+          .attr("y", (d) => d.y0) 
+          .text((d) => d.data.name)
+          .attr("font-size", "15px")
+          .attr("fill", "white")
     }
   }
 
